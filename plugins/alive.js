dd57fcde628feefd5efa717af06664f021d56e70
NETHA Tech <150256662+Nethmikakaushalyaherath@users.noreply.github.com>
2024-08-27 22:27:41 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -10,23 +10,21 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 
-let des = `*👋 HELLO, I'm QUEEN-NETHU-MD I'm Alive Now*
+let des = `*👋 HELLO I'm QUEEN-NETHU-MD I'm Alive Now 🍂✨*
 
-*🧑‍💻 Owner* - NETHMIKA KAUSHALYA(SL NETHUMAX)
+*🧑‍💻 Owner* - NETHMIKA KAUSHALYA
 
 *📋 Owner Number* - 94704227534
 
 *📃 Tipe .menu To Get menu list*
 
-*SUBSCRIBE MY YOUTUBE CHANNEL*
+*➤ SUBSCRIBE MY YOUTUBE CHANNEL*
 
 🔗◦ https://www.youtube.com/@SlNethuMax
 
-*JOIN MY WHATSAPP CHANNEL*
+*➤ FOLLOW MY WHATSAPP CHANNEL*
 
 📎◦ https://whatsapp.com/channel/0029VagCogPGufJ3kZWjsW3A
-
-*ᴛʜᴀɴᴋꜱ ꜰᴏʀ = ꜱʜᴀᴅᴏᴡ-ᴍᴅ💗🍃*
 
 *©ᴘᴏᴡᴇʀᴇᴅ ʙʏ ɴᴇᴛʜᴜ ᴍᴀx*`
 return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: des},{quoted: mek})
