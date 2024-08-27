2ee16dc505ac1d84281c3bd0c7c1d897df5a9235
QUEEN KYLIE - MD🔰 <mdshadow692@gmail.com>
2024-08-27 16:03:35 +0530

    Create system.js

---
+++ plugins/system.js
@@ -0,0 +1,29 @@
+const {cmd , commands} = require('../command')
+const os = require("os")
+const {runtime} = require('../lib/functions')
+
+cmd({
+    pattern: "system",
+    alias: ["status","botinfo"],
+    desc: "check up time",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{ 
+
+let status =`┌────────────────────
+    *├ ⏳Uptime:-  ${runtime(process.uptime())}*    
+    *├ 📟Ram usage:- ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB*
+    *├ ⚙️HostName:- ${os.hostname()}*
+    *├ 👨‍💻Owner :- Sahas Tech*   
+    *├ 🤖version: 1.5.0*
+    └───────────────────────
+    > *ᴘᴏᴡᴇʀᴇᴅ ʙʏ ꜱᴀʜᴀꜱ ᴛᴇᴄʜ*
+`
+return reply(`${status}`)
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
