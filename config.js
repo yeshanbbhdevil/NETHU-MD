80e3adeddc7e6caedf4843168c07b7d42583ba0a
NETHA Tech <150256662+Nethmikakaushalyaherath@users.noreply.github.com>
2024-08-27 22:23:51 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "put sesson id",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/9b44e3266c065df601430.jpg",
-ALIVE_MSG: process.env.ALIVE_IMG || "🤖🔰 Hi QUEEN-KYLIE-MD Is Online Now 💻\n*💻 Owner* - QUEEN-KYLIE-MD\n\n*💻 Owner Number* -94718913389\n\n_විධාන මෙනුව ලබා ගැනීමට .menu ලෙස ටයිප් කරන්න._",
-SUDO_NB: process.env.SUDO_NB || "ඔයාගෙ owner number එක දෙන්න",
+ALIVE_MSG: process.env.ALIVE_IMG || "🤖🔰 Hi QUEEN-NETHU-MD Is Online Now 💻\n*💻 Owner* - QUEEN-NETHU-MD*💻 Owner Number* -94704227534\n\n_විධාන මෙනුව ලබා ගැනීමට .menu ලෙස ටයිප් කරන්න._",
+SUDO_NB: process.env.SUDO_NB || "+94704227534",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-MODE: process.env.MODE || "public",
+MODE: process.env.MODE || "private",
 };
