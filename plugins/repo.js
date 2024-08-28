48cb02e4b3aef00190ec04d196b682d788f803b1
NETHA Tech <150256662+Nethmikakaushalyaherath@users.noreply.github.com>
2024-08-28 22:19:39 +0530

    Update repo.js

--- plugins/repo.js
+++ plugins/repo.js
@@ -4,29 +4,32 @@ cmd({
     pattern: "repo",
     desc: "repo the bot",
     category: "main",
+    react: "📡",
     filename: __filename
 },
 
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 
-let dec = `*𝙌𝙐𝙀𝙀𝙉-𝙉𝙀𝙏𝙃𝙐-𝙈𝘿 𝙍𝙚𝙥𝙤𝙨𝙞𝙩𝙤𝙧𝙮 𝙄𝙣𝙛𝙤𝙧𝙢𝙖𝙩𝙞𝙤𝙣*
+let dec = `*🧚‍♂️ 𝐐𝐔𝐄𝐄𝐍 𝐍𝐄𝐓𝐇𝐔 𝐌𝐃 𝐑𝐞𝐩𝐨𝐬𝐢𝐭𝐨𝐫𝐲 𝐈𝐧𝐟𝐨𝐫𝐦𝐚𝐭𝐢𝐨𝐧*
 
-*🤖 NAME*: QUEEN-NETHU-MD
+*🤖 ɴᴀᴍᴇ: Qᴜᴇᴇɴ-ɴᴇᴛʜᴜ-ᴍᴅ*
+*🧑‍💻 ᴏᴡɴᴇʀ: ɴᴇᴛʜᴍɪᴋᴀ ᴋᴀᴜꜱʜᴀʟʏᴀ (ꜱʟ ɴᴇᴛʜᴜ ᴍᴀx)*
+*🧬 ᴠᴇʀꜱɪᴏɴ: 1.0.0*
 
-*🧑‍💻 OWNER*: NETHMIKA KAUSHALYA
 
-*➤ REPO LINK*
+*📡 REPO LINK*
 
 🔗◦https://github.com/Nethmikakaushalyaherath/QUEEN-NETHU-MD/tree/main
 
-*➤ SUBSCRIBE MY YOUTUBE CHANNEL*
+*📌 SUBSCRIBE MY YOUTUBE CHANNEL*
 
 🔗◦ https://www.youtube.com/@SlNethuMax
 
 *➤ FOLLOW MY WHATSAPP CHANNEL*
 
 🔗◦ https://whatsapp.com/channel/0029VagCogPGufJ3kZWjsW3A
+
 
 *©ᴘᴏᴡᴇʀᴇᴅ ʙʏ ɴᴇᴛʜᴜ ᴍᴀx*
 `
