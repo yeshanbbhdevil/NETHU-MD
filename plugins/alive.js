0914464eb998a8c7cea2a4102cb9b6b1b6634c62
NETHA Tech <150256662+Nethmikakaushalyaherath@users.noreply.github.com>
2024-08-28 14:44:23 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -5,6 +5,7 @@ cmd({
     pattern: "alive",
     desc: "Check bot online or no.",
     category: "main",
+    react: "👋",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
