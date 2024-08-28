cd58b288e18ad68400b1c207b21dceb1383f026f
NETHA Tech <150256662+Nethmikakaushalyaherath@users.noreply.github.com>
2024-08-28 14:32:03 +0530

    Update system.js

--- plugins/system.js
+++ plugins/system.js
@@ -12,14 +12,15 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{ 
 
-let status =` ┌────────────────────
+let status =` 
+    ┌────────────────────
     *├ ⏳Uptime:-  ${runtime(process.uptime())}*    
     *├ 📟Ram usage:- ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB*
     *├ ⚙️HostName:- ${os.hostname()}*
     *├ 👨‍💻Owner :- Nethu Max*   
-    *├ 🤖version: 1.0.0*
+    *├ 🤖Version: 1.0.0*
     └───────────────────────
-    > *©ᴘᴏᴡᴇʀᴇᴅ ʙʏ ɴᴇᴛʜᴜ ᴍᴀx*
+     *©ᴘᴏᴡᴇʀᴇᴅ ʙʏ ɴᴇᴛʜᴜ ᴍᴀx*
 `
 return reply(`${status}`)
 }catch(e){
