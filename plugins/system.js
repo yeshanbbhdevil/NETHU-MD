0ca3f4242a66b6ca7635b1b30a61645a084db808
NETHA Tech <150256662+Nethmikakaushalyaherath@users.noreply.github.com>
2024-08-28 22:06:11 +0530

    Update system.js

--- plugins/system.js
+++ plugins/system.js
@@ -7,6 +7,7 @@ cmd({
     alias: ["status","botinfo"],
     desc: "check up time",
     category: "main",
+    react: "🧬",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
