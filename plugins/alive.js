eef9fcb87e600bfd0838896c97943b82a5387996
NETHA Tech <150256662+Nethmikakaushalyaherath@users.noreply.github.com>
2024-08-29 13:56:15 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -13,13 +13,13 @@ try{
 
 let des = `👋 𝙷𝚎𝚕𝚕𝚘 ${pushname} 𝙸'𝚖 𝚊𝚕𝚒𝚟𝚎 𝚗𝚘𝚠
 
-*👾 Im QUEEN-NETHU-MD whatsapp bot*`
+*👾 Im QUEEN-NETHU-MD whatsapp bot*
 
 🐼This is the result of our teams hard work
 and our technical cybers team owns the bots
-rights and code rights. Therefore, you have no
+rights and code rights.Therefore you have no
 chance to change and submit our bot under
-any circumstances And 500 Commands And
+any circumstances And 250 Commands And
 logo, thumbnail,banner Maker Commands Al
 Chatbot feathers On Our Bot
 
