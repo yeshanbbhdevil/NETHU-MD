564d6a8290f98c3297ef5e1c332d59f39c17e8de
NETHA Tech <150256662+Nethmikakaushalyaherath@users.noreply.github.com>
2024-08-30 20:30:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -146,6 +146,13 @@ if(isReact) return
 m.react("👨‍💻")
 }
 
+if (config.AUTO_VOICE === 'true') {
+const url = 'https://raw.githubusercontent.com/DarkYasiyaofc/VOICE/main/Voice-Raw/FROZEN-V2'
+let { data } = await axios.get(url)
+for (vr in data){
+if((new RegExp(`\\b${vr}\\b`,'gi')).test(body)) conn.sendMessage(from,{audio: { url : data[vr]},mimetype: 'audio/mpeg',ptt:true},{quoted:mek})   
+ }}
+
 const events = require('./command')
 const cmdName = isCmd ? body.slice(1).trim().split(" ")[0].toLowerCase() : false;
 if (isCmd) {
