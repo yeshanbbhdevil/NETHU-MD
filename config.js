2c397b63d44f9192ca1afb4f101a5c73367b1fa3
NETHA Tech <150256662+Nethmikakaushalyaherath@users.noreply.github.com>
2024-08-30 20:35:07 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,9 +6,9 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "put sesson id",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/9420d31d752ae858163cd.jpg",
-ALIVE_MSG: process.env.ALIVE_IMG || "Hello NETHU-MD Is Online Now",
-SUDO_NB: process.env.SUDO_NB || "+94704227534",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/6d986fa37cb22f8e6717a.jpg",
+SUDO_NB: process.env.SUDO_NB || "ඔයාගෙ owner number එක දෙන්න",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-MODE: process.env.MODE || "private",
+MODE: process.env.MODE || "public",
+AUTO_VOICE: "true"
 };
