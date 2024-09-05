fb1fcd3d4da0c9a3624c58c8ac254d3747e46f5c
Bot024 <botn84184@gmail.com>
2024-09-05 21:52:36 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,9 +5,9 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "jbpgSSxI#Ov-eUBXsnU3mtZllvMyc-7eS6jpSDuW_0-xb3LV6i9s",
+SESSION_ID: process.env.SESSION_ID || "IvcgxZAa#C7wAUz6FPdqhk5bCUZkDYm8ZfNw6EuLcjkDnpk3UWe4",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/7f0d7a04a30a602307e3d.jpg",
-SUDO_NB: process.env.SUDO_NB || "ඔයාගෙ owner number එක දෙන්න",
+SUDO_NB: process.env.SUDO_NB || "94715922193",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: "true"
