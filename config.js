80a295c0238b3b8c9110a1919d6da22c61388a06
yeshanbbhdevil <yeshanbasnayake20071001@gmail.com>
2024-10-18 12:55:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,10 +5,10 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "IvcgxZAa#C7wAUz6FPdqhk5bCUZkDYm8ZfNw6EuLcjkDnpk3UWe4",
+SESSION_ID: process.env.SESSION_ID || "JYpBkCSa#9zPRFpR6fTMkgRU3S0Q4pa6ZbT76kMBzbxH2i9Q6iLE",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/7f0d7a04a30a602307e3d.jpg",
-SUDO_NB: process.env.SUDO_NB || "94715922193",
+SUDO_NB: process.env.SUDO_NB || "94701219309",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-MODE: process.env.MODE || "public",
+MODE: process.env.MODE || "private",
 AUTO_VOICE: "true"
 };
